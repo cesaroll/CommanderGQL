3c4395b8fac4dc5601404cf4a59cd2b6bd3a222b
Cesar Lopez <cesarlerdeut@gmail.com>
2021-12-19 02:16:35 +0100

    Configure DB

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CommanderGQL.Data;
 
 namespace CommanderGQL
 {
@@ -24,7 +26,8 @@ namespace CommanderGQL
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-        
+      services.AddDbContext<AppDbContext>(opt =>
+      opt.UseSqlServer(Configuration.GetConnectionString("CommandConStr")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
